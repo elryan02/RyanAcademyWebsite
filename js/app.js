d275c67b6c68404a9b956c207fd6dcb79bd049a8
Emma Ryan <Lkyemmaryan@gmail.com>
2017-11-13 16:32:33 -0500

    worked on about and classes.

--- js/app.js
+++ js/app.js
@@ -1,28 +1,29 @@
+
 $(document).ready(function() {
 
-// Get the modal
-var formModal = document.getElementById('formModal');
+var formModal = document.getElementById("formModal");
 
-// Get the button that opens the modal
 var regBtn = document.getElementById("regBtn");
 
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("formClose")[0];
 
-// When the user clicks on the button, open the modal
 regBtn.onclick = function() {
     formModal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     formModal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == formModal) {
         formModal.style.display = "none";
     }
 }
 });
+
+// Hover overlay
+$document.ready(function() {
+  var overlaySecondary = document.getElementsByClassName("imgOverlay");
+
+})

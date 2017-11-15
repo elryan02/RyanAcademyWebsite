db5c2250b9f65aa411a38f065a40610c039101a0
Emma Ryan <Lkyemmaryan@gmail.com>
2017-11-14 23:06:11 -0500

    added js file to base level of directory

---
+++ app.js
@@ -0,0 +1,31 @@
+// Popup Form
+
+var formModal = document.getElementById("formModal");
+
+var regBtn = document.getElementById("regBtn");
+
+$("#regBtn").click(function(){
+    formModal.style.display = "block";
+});
+
+// Closing Popup Form
+
+var span = document.getElementsByClassName("formClose")[0];
+
+$("span").click(function(){
+    formModal.style.display = "none";
+});
+
+$(document).click(function(){
+    if (event.target == formModal) {
+      formModal.style.display = "none";
+    };
+});
+
+// Submit Button
+
+var formSubmit = document.getElementById("formSubmit");
+
+$("#formSubmit").click(function(){
+    alert("Congratulations! You just submitted something into the void!");
+});
